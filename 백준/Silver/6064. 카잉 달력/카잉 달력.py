4f312bbd0ca5ef3e116c84d4fa7f412f77721c85
Jihyeon Choi <80878955+gus1043@users.noreply.github.com>
2025-06-09 08:28:15 +0900

    [Silver I] Title: 카잉 달력, Time: 3724 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -0,0 +1,25 @@
+import sys
+# 데이터의 입력을 나태내는 정수 T
+T = int(sys.stdin.readline())
+
+for i in range(T):
+    M, N, x, y = map(int, sys.stdin.readline().split())
+    temp=[0,0]
+    count=0
+    
+    #x를 기준으로 맞춰보자
+    year = x
+    found = False
+    while year <= M*N:
+        if (year-1)%N+1 == y:
+            found=True
+            print(year)
+            break
+        year+=M
+
+    if not found:
+        print(-1)
+
+
+
+
