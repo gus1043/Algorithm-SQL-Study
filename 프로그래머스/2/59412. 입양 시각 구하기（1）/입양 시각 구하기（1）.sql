97c67295bf2bfc7205d73547ee3fd870a4c43f5f
Jihyeon Choi <80878955+gus1043@users.noreply.github.com>
2025-06-10 08:54:46 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT  TO_NUMBER(TO_CHAR(DATETIME, 'HH24')) AS HOUR, COUNT(*) AS COUNT
+from ANIMAL_OUTS
+where TO_CHAR(DATETIME, 'HH24') between 09 and 19
+GROUP BY TO_CHAR(DATETIME, 'HH24')
+order by hour asc
