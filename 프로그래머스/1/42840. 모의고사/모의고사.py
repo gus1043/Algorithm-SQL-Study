1d68da1de6b30bb815c1489eaf41a610af74dd71
Jihyeon Choi <80878955+gus1043@users.noreply.github.com>
2025-08-20 21:19:11 +0900

    [level 1] Title: 모의고사, Time: 4.99 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,31 +1,19 @@
 def solution(answers):
     answer = []
+    patterns = [[ 1, 2, 3, 4, 5],
+               [2, 1, 2, 3, 2, 4, 2, 5],
+               [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    scores = [0]*3
     
-    first = [1,2,3,4,5]*2000
-    second = [2,1,2,3,2,4,2,5]*1250
-    thrid = [3,3,1,1,2,2,4,4,5,5]*1000
+    for i, answer in enumerate(answers):
+        for j, pattern in enumerate(patterns):
+            if answer == pattern[i % len(pattern)]:
+                scores[j]+=1
+    max_score = max(scores)
     
-    #answers의 길이로 다 잘라보자
-    first=first[:len(answers)]
-    second=second[:len(answers)]
-    third=thrid[:len(answers)]
-    
-    #맞는 개수를 찾아야해
-    first_correct=0
-    second_correct=0
-    third_correct=0
-    for i in range(len(answers)):
-        if answers[i]==first[i]:
-            first_correct+=1
-        if answers[i]==second[i]:
-            second_correct+=1
-        if answers[i]==third[i]:
-            third_correct+=1
-    
-    max_= max(first_correct, second_correct, third_correct)
-    
-    if first_correct == max_ : answer.append(1)
-    if second_correct == max_ : answer.append(2)
-    if third_correct == max_ : answer.append(3)
-        
-    return answer
+    highest_score = []
+    for i, score in enumerate(scores):
+        if max_score == score:
+            highest_score.append(i+1)
+            
+    return highest_score
