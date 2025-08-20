26f48dae100fab15083db80a5338691d39a7663c
Jihyeon Choi <80878955+gus1043@users.noreply.github.com>
2025-08-20 22:56:17 +0900

    [level 2] Title: 행렬의 곱셈, Time: 107.44 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/README.md
+++ 프로그래머스/2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.8 MB, 시간: 73.15 ms
+메모리: 10 MB, 시간: 107.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 24일 15:47:00
+2025년 08월 20일 22:56:15
 
 ### 문제 설명
 
--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,17 +1,12 @@
-def solution(arr1, arr2):
-    answer = [[0] * len(arr2[0]) for _ in range(len(arr1))]
+def solution(arr1, arr2):  
+    r1, c1 = len(arr1), len(arr1[0])
+    r2, c2 = len(arr2), len(arr2[0])
     
-    #arr1의 행(hor)이랑 열(ver) 갯수를 알아보자
-    hor=len(arr1)
-    ver=len(arr2[0])
-     
-    for i in range(hor): #행 번호 할당
-        for j in range(ver): #열 번호 할당
-            tmp=0
-            #arr1의 행 번호 전체랑 arr2의 열 번호 전체가 곱해지고 더해진다.
-            for k in range(len(arr2)):
-                # print(arr1[i][k],"*",arr2[k][j])
-                tmp+=arr1[i][k]*arr2[k][j]
-            answer[i][j]=tmp
+    result = [[0]*c2 for _ in range(r1)]
+    
+    for i in range(r1):
+        for j in range(c2):
+            for k in range(c1):
+                result[i][j]+=arr1[i][k]*arr2[k][j]
 
-    return answer
+    return result
