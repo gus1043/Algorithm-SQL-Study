34c698b491c390f8fb7e5506afe524536a6856a8
Jihyeon Choi <80878955+gus1043@users.noreply.github.com>
2025-10-14 14:21:26 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.cpp
+++ 프로그래머스/1/12901. 2016년/2016년.cpp
@@ -5,18 +5,15 @@ using namespace std;
 
 string solution(int a, int b) {
     string answer = "";
+    vector <string> weekdays = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+    vector <int> days = {31, 29, 31, 30,31, 30,31,31, 30,31, 30,31};
     
-    //2016년 1월 1일은 금요일
-    vector<string> weeks = {"FRI","SAT", "SUN", "MON","TUE","WED","THU"};
-    vector<int> days = {31,29,31,30,31,30,31,31,30,31,30,31};
-    
-    int temp=b-1;
-    
-    //n월을 만들자
+    //전체 날짜 변화를 더하고 나머지 값으로 일자 구하기
+    int total = b-1; //1일 하나 빼고 가기
     for (int i=0;i<a-1;i++){
-        temp+=days[i];
+        total+=days[i];
     }
     
-    answer = weeks[temp%7];
+    answer = weekdays[total%7];
     return answer;
 }
--- 프로그래머스/1/12901. 2016년/README.md
+++ 프로그래머스/1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
 
 ### 성능 요약
 
-메모리: 4.19 MB, 시간: 0.01 ms
+메모리: 4.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 04일 22:52:05
+2025년 10월 14일 14:21:22
 
 ### 문제 설명
 
