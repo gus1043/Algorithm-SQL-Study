cb71babe73ace7e32ea32cc116194641bfb5b6f3
Jihyeon Choi <80878955+gus1043@users.noreply.github.com>
2025-10-21 09:00:49 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 4.09 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int age) {
+    int answer = 0;
+    //2022년 기준 나이 주어졌을때 몇년생?
+    answer = 2022-age+1;
+    return answer;
+}
