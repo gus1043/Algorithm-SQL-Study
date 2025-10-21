a31347d9393bdb68c19ac1286d92901362cdbe34
Jihyeon Choi <80878955+gus1043@users.noreply.github.com>
2025-10-21 10:18:25 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 4.06 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    string num = to_string(n);
+    
+    for(char c: num){
+        answer += c-'0';
+    }
+    return answer;
+}
