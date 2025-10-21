dd35291f89ab70d3d2ca150e22661bbfd59cbb1d
Jihyeon Choi <80878955+gus1043@users.noreply.github.com>
2025-10-21 09:51:13 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 3.74 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+
+int solution(int n) {
+    int m = (int)sqrt(n);
+    if(m *m == n){
+        return 1;
+    }return 2;
+}
