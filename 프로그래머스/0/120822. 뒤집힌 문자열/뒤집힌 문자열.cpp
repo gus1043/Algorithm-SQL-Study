fd0fde9aa972f7a58678623da8e1a0cc12382bec
Jihyeon Choi <80878955+gus1043@users.noreply.github.com>
2025-10-21 09:15:52 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string) {
+    reverse(my_string.begin(), my_string.end());
+    string answer = my_string;
+    return answer;
+}
