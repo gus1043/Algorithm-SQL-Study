a4c74284ccb322c0e07f2f40a0c81d2bb682ce49
Jihyeon Choi <80878955+gus1043@users.noreply.github.com>
2025-10-22 16:52:48 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    string odd = "";
+    string even = "";
+    
+    for (int i=0; i<num_list.size();i++){
+        if( num_list[i]%2==0 ){
+            odd += to_string(num_list[i]);
+        }else {even += to_string(num_list[i]); }
+    }
+    
+    answer = stoi(odd)+stoi(even);
+    return answer;
+}
