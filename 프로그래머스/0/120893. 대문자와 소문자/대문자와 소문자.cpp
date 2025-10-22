b48a9b834e7b80a869cdb62a22ae4ea81d27161d
Jihyeon Choi <80878955+gus1043@users.noreply.github.com>
2025-10-22 17:19:27 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    for (int i=0;i<my_string.length(); i++){
+        if(isupper(my_string[i])) answer+=tolower(my_string[i]);
+        else answer+=toupper(my_string[i]);
+    }
+    return answer;
+}
