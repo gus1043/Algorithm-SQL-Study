c818a9f23b9359eac3741bfaaed79d2b1a483e71
Jihyeon Choi <80878955+gus1043@users.noreply.github.com>
2025-10-27 15:28:14 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> wallet, vector<int> bill) {
+    int answer = 0;
+    sort(wallet.begin(), wallet.end());
+    sort(bill.begin(), bill.end());
+    while(wallet[1]<bill[1] ||wallet[0]<bill[0]){
+       
+        if(bill[0]>wallet[0] || bill[1]>wallet[1]){
+            bill[1]/=2;
+            answer++;
+            sort(bill.begin(), bill.end());
+        }
+    }
+    return answer;
+}
