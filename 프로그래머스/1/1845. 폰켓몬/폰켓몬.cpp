b9352ae2f9ea3966adee983e99e4f84c51c15479
Jihyeon Choi <80878955+gus1043@users.noreply.github.com>
2025-11-03 10:16:15 +0900

    [level 1] Title: 폰켓몬, Time: 0.86 ms, Memory: 4.09 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,31 +1,11 @@
 #include <vector>
-#include <map>
-
+#include <unordered_set>
 using namespace std;
 
 int solution(vector<int> nums)
 {
     int answer = 0;
+    unordered_set<int> s(nums.begin(), nums.end());
     
-    // 가질 수 있는 폰켓몬의 수 
-    int count = nums.size()/2;
-    
-    map<int, int> mon;
-    
-    for (int i=0; i<nums.size();i++){
-        if( mon.find(nums[i])==mon.end()){
-            mon[nums[i]]=1;
-        }else{
-            mon[nums[i]]+=1;
-        }
-    }
-    
-    int type = mon.size();
-    
-    if(type>= count){
-        answer = count;
-    }else{
-        answer = type;
-    }
-    return answer;
+    return min(nums.size()/2, s.size());
 }
