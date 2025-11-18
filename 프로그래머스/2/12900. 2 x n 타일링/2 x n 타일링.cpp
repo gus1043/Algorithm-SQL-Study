c010a46285afab6fafdfd3c3995b783feec13a01
Jihyeon Choi <80878955+gus1043@users.noreply.github.com>
2025-11-18 09:29:27 +0900

    [level 2] Title: 2 x n 타일링, Time: 0.44 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+long long solution(int n) {
+    if(n==1){
+        return 1;
+    }
+    
+    if(n==2){
+        return 2;
+    }
+    
+    vector<long long> dp(n+1, 0);
+    dp[1]=1;
+    dp[2]=2;
+    
+    for(int i=3;i<=n;++i){
+        dp[i]=(dp[i-1]+dp[i-2])%1000000007;
+    }
+    
+    return dp[n];
+}
